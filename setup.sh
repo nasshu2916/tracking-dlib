78d87abbd8d63651f8d3ff0aec07b315ab567d9a
naoyakohda <nashu5667@gmail.com>
2021-05-01 09:19:08 +0900

    run format

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
+#! /bin/bash
+
 mkdir -p model
-cd model
+cd model || exit 1
 wget http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2
 bunzip2 shape_predictor_68_face_landmarks.dat.bz2
 pip install -r requirements.txt
